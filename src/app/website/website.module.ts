e9f18675f1334b29b7800feb3e9f683278e4b140
Jackseed <vinkho.c@gmail.com>
2019-02-23 15:13:06 +0100

    homepage fixed

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiModule } from '../layout/ui.module';
 import { AppRoutingModule } from '../app-routing.module';
+import { ProjectPageComponent } from './project-page/project-page.component';
 
 @NgModule({
   declarations: [
-  ],
+  ProjectPageComponent],
   imports: [
     CommonModule,
     UiModule,
