0b2c3e8a6b427cdbc24f016e6d3a95756a6c2dd5
Jackseed <vinkho.c@gmail.com>
2019-02-25 09:02:59 +0100

    project page content

--- src/app/models/project-list.ts
+++ src/app/models/project-list.ts
@@ -3,7 +3,21 @@ export interface Projet {
   titre: string;
   lien: string;
   description: string[];
+  signature_projet: string;
   images: string[];
+  legende1: string[];
+  images1: string[];
+  legende2: string[];
+  images2: string[];
+  legende3: string[];
+  images3: string[];
+  legende4: string[];
+  images4: string[];
+  legende5: string[];
+  images5: string[];
+  legende_finale: string[];
+  copyright: string;
+  signature: string;
 }
 
 export const PROJETS = [
@@ -14,10 +28,24 @@ export const PROJETS = [
       'Conception d\'un supplément hebdomadaire au journal Le Monde.',
       'Le Monde des Enfants, pour expliquer l\'actualité aux enfants avec des mots simples : C\'est où la  Syrie ?',
 // tslint:disable-next-line: max-line-length
-      'Et pourquoi on en parle ? Papa, un coeur greffé il peut tomber amoureux ? Et pourquoi il fait si froid à Chigaco alors qu\'on dit que la terre elle se réchauffe ?',
-      '',
-      'La Cambre, 2014.'],
-    images: ['lmde_1.jpg', 'lmde_2.jpg', 'lmde_3.jpg', 'lmde_4.jpg', 'lmde_5.jpg', 'lmde_6.jpg']
+      'Et pourquoi on en parle ? Papa, un coeur greffé il peut tomber amoureux ? Et pourquoi il fait si froid à Chigaco alors qu\'on dit que la terre elle se réchauffe ?'
+    ],
+    signature_projet: 'La Cambre, 2014.',
+    images: ['lmde_1.jpg', 'lmde_2.jpg', 'lmde_3.jpg', 'lmde_4.jpg', 'lmde_5.jpg', 'lmde_6.jpg'],
+    legende1: ['Version web'],
+    images1: ['lmde-laptop1.jpg', 'lmde-laptop2.jpg', 'lmde-laptop3.jpg'],
+    legende2: [''],
+    images2: [''],
+    legende3: [''],
+    images3: [''],
+    legende4: [''],
+    images4: [''],
+    legende5: [''],
+    images5: [''],
+// tslint:disable-next-line: max-line-length
+    legende_finale: ['/!\\ Ce que je n\'ai pas dessiné ou écrit moi-même pour ce projet a été emprunté à 1jour1actu, Jacques Azam, Marie Assénat, Hector Dexet. Ce projet ne sera diffusé nulle part ailleurs sans leur autorisation préalable. Merci à eux.'],
+    copyright: 'Graphisme - Illustration',
+    signature: 'Lena Piroux 2016'
     },
     /*
     { section: 'graphisme', titre: 'College 75', lien: 'college75' },
