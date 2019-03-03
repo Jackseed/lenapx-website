c28cb88f0ff2130d4ca79de7b58f8a29e931e8f9
Jackseed <vinkho.c@gmail.com>
2019-03-03 22:09:03 +0100

    WIP: xs-menu

--- src/app/layout/ui.module.ts
+++ src/app/layout/ui.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
 
   exports: [
     FlexLayoutModule,
-    MatGridListModule
+    MatGridListModule,
+    MatToolbarModule
   ]
 })
 export class UiModule { }
