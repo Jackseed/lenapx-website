9c01f1b2bbd719f30c3d7f26b2c0dc191d021b30
Jackseed <vinkho.c@gmail.com>
2019-03-11 09:20:50 +0100

    WIP: responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PROJETS, Projet } from './models/project-list';
 import { ImageTitre, TITRES_MENU } from './models/titres-menu';
+import { ObservableMedia } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,6 @@ import { ImageTitre, TITRES_MENU } from './models/titres-menu';
 export class AppComponent {
   public projets: Projet[] = PROJETS;
   public imageTitres: ImageTitre[] = TITRES_MENU;
+
+  constructor(public media: ObservableMedia) {}
 }
--- src/app/website/grid/grid.component.ts
+++ src/app/website/grid/grid.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { VIGNETTES } from 'src/app/models/vignettes';
+import { ObservableMedia } from '@angular/flex-layout';
+
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 export interface Vignette {
   image: string;
@@ -14,11 +18,34 @@ export interface Vignette {
 })
 export class GridComponent implements OnInit {
 
+  public cols: Observable<number>;
   public vignettes: Vignette[] = VIGNETTES;
 
-  constructor() { }
+  constructor(
+    public media: ObservableMedia,
+  ) { }
 
   ngOnInit() {
+    const grid = new Map([
+      ['xs', 2],
+      ['sm', 3],
+      ['md', 4],
+      ['lg', 4],
+      ['xl', 4]
+    ]);
+    let start: number;
+    grid.forEach((cols, mqAlias) => {
+      if (this.media.isActive(mqAlias)) {
+        start = cols;
+      }
+    });
+    this.cols = this.media.asObservable().pipe(
+      map(change => {
+        console.log(change);
+        console.log(grid.get(change.mqAlias));
+        return grid.get(change.mqAlias);
+      }),
+      startWith(start)
+      );
   }
-
 }
