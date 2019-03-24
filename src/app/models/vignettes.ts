b1b578413474f2dbf68fd67e7278988050493547
Jackseed <vinkho.c@gmail.com>
2019-03-24 20:15:57 +0100

    f*** wip

--- src/app/models/vignettes.ts
+++ src/app/models/vignettes.ts
@@ -1,18 +1,4 @@
 export const VIGNETTES = [
-    {image: 'vignette-paulette.jpg', alt: 'Paulette', lien: 'pauletteweb'},
-    {image: 'vignette-illustrations2.jpg', alt: 'Illustrations 2', lien: 'illustrations2'},
-    {image: 'vignette-carto.jpg', alt: 'Cartoactium', lien: 'cartoactium'},
-    {image: 'vignette-couvertures.jpg', alt: 'Collection XXI', lien: 'collectionxxi'},
-    {image: 'vignette-cfs.jpg', alt: 'Chill, Farm & Sun', lien: 'chillfarmandsun'},
-    {image: 'vignette-contact.jpg', alt: 'Illustrations 1', lien: 'illustrations1'},
-    {image: 'vignette-monsters.jpg', alt: 'Sérigraphie', lien: 'serigraphie'},
-    {image: 'vignette-gifpaulette.gif', alt: 'Paulette Gif', lien: 'paulettegif'},
-    {image: 'vignette-GIF-yeux.gif', alt: 'Gifs', lien: 'gifs'},
-    {image: 'vignette-feelslikehome.jpg', alt: 'Feels like home', lien: 'feelslikehome'},
-    {image: 'vignette-pictos.jpg', alt: 'Pictos bateaux', lien: 'pictosbateaux'},
-    {image: 'vignette-illustrations3.jpg', alt: 'Illustrations 3', lien: 'illustrations3'},
-    {image: 'vignette-illustrations1.jpg', alt: 'Illustrations 1', lien: 'illustrations1'},
-    {image: 'vignette-m2.jpg', alt: 'm2', lien: 'm2'},
-    {image: 'vignette_paulette.jpg', alt: 'Paulette', lien: 'paulette'},
-    {image: 'vignette-saul.jpg', alt: 'Saul Bass Walk', lien: 'saulbasswalk'}
+    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},
+    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},    {image: 'vignette-couvertures.jpg', alt: 'Paulette', lien: 'pauletteweb'},
   ];
--- src/app/website/grid/grid.component.ts
+++ src/app/website/grid/grid.component.ts
@@ -3,7 +3,7 @@ import { VIGNETTES } from 'src/app/models/vignettes';
 import { ObservableMedia } from '@angular/flex-layout';
 
 import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { map, startWith, tap } from 'rxjs/operators';
 
 export interface Vignette {
   image: string;
@@ -20,30 +20,35 @@ export class GridComponent implements OnInit {
 
   public cols: Observable<number>;
   public vignettes: Vignette[] = VIGNETTES;
+  public mediaWidth: string;
 
   constructor(
-    public media: ObservableMedia,
+    public media$: ObservableMedia,
   ) { }
 
   ngOnInit() {
     const grid = new Map([
       ['xs', 2],
-      ['sm', 3],
+      ['sm', 4],
       ['md', 4],
       ['lg', 4],
       ['xl', 4]
     ]);
     let start: number;
     grid.forEach((cols, mqAlias) => {
-      if (this.media.isActive(mqAlias)) {
+      if (this.media$.isActive(mqAlias)) {
         start = cols;
+        this.mediaWidth = mqAlias;
       }
     });
-    this.cols = this.media.asObservable().pipe(
+    this.cols = this.media$.asObservable().pipe(
+      tap(change => this.mediaWidth = change.mqAlias),
       map(change => {
         return grid.get(change.mqAlias);
       }),
       startWith(start)
       );
   }
+
+
 }
